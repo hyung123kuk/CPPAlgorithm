163ae36f8aae2cc0326fa8b42a7b09c32cecc78d
hyungkuk <im06@naver.com>
2024-01-15 20:11:22 +0900

    환경 설정

---
+++ Maze/Maze.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include <iostream>
+#include "ConsoleHelper.h"
+
+int main()
+{
+
+	uint64 lastTick = 0;
+
+	while (true)
+	{
+		const uint64 currentTick = ::GetTickCount64();
+		const uint64 deltaTick = currentTick - lastTick;
+		lastTick = currentTick;
+
+		ConsoleHelper::SetCursorPosition(0, 0);
+		ConsoleHelper::ShowConsoleCursor(false);
+		ConsoleHelper::SetCursorColor(ConsoleColor::RED);
+
+		const char* TILE = "бс ";
+		for (int32 y = 0; y < 25; y++)
+		{
+			for (int32 x = 0; x < 25; x++)
+			{
+				cout << TILE;
+			}
+			cout << endl;
+		}
+
+	}
+}
+
