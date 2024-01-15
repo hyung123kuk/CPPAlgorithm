8c40586f291498e24e2261d288cb4bab89b1adf6
hyungkuk <im06@naver.com>
2024-01-15 21:34:31 +0900

    fix. 벡터 (동적 배열) 구현 연습

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,8 +1,84 @@
-
 #include <iostream>
+#include <vector>
+using namespace std;
+
+
+template<typename T>
+class Vector
+{
+public:
+    Vector()
+    {
+
+    }
+
+    ~Vector()
+    {
+        if (_data)
+            delete[] _data;
+    }
+
+    void push_back(const T& value)
+    {
+        if (_size == _capacity)
+        {
+            int newCapacity = static_cast<int>(_capacity * 1.5f);
+            if (newCapacity == _capacity)
+                newCapacity++;
+
+            reserve(newCapacity);
+
+        }
+        _data[_size] = value;
+
+        _size++;
+    }
+
+    void reserve(int capacity)
+    {
+        if (_capacity >= capacity)
+            return;
+
+        _capacity = capacity;
+
+        T* newData = new T[_capacity];
+
+        for (int i = 0; i < _size; i++)
+            newData[i] = _data[i];
+        
+        if (_data)
+            delete[] _data;
+
+        _data = newData;
+
+    }
+
+    T& operator[](const int pos) { return _data[pos]; }
+
+    int size() { return _size; }
+    int capacity() { return _capacity; }
+
+    void clear()
+    {
+        if (_data)
+        {
+            delete[] _data;
+            _data = new T[_capacity];
+        }
+
+        _size = 0;
+    }
+
+
+private:
+    T* _data = nullptr;
+    int _size = 0;
+    int _capacity = 0;
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
 
