bb5c498cbc70402315c1d892afaf7818ba124146
hyungkuk <im06@naver.com>
2024-01-15 22:05:20 +0900

    fix. Container를 이용한 Stack 구현

---
+++ Algorithm/Stack.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+using namespace std;
+
+template<typename T, typename Container = vector<T>>
+class Stack
+{
+public:
+	void push(const T& value)
+	{
+		_container.push_back(value);
+	}
+
+	void pop()
+	{
+		_container.pop_back();
+	}
+
+	T& top()
+	{
+		return _container.back();
+	}
+
+	bool empty() { return _container.empty(); }
+	int size() { return _container.size();}
+
+private:
+	Container _container;
+
+};
